7292600b8def90c49ae7de561aab259c75e0d137
Rodrigo Davalos <roddavalos97@gmail.com>
2025-10-02 00:20:02 -0600

    Update cities dictionary with additional cities
    Added Mexico City, Guadalajara, and Monterrey to the cities dictionary.

--- cities.py
+++ cities.py
@@ -1,71 +1,77 @@
 cities = {
     # 🌱 Growers (Mexico)
-    "Tuxtla Gutierrez": (16.7510, -93.1160, "America/Mexico_City"),  # MX
-    "Ensenada": (31.8667, -116.6000, "America/Tijuana"),  # MX
-    "Celaya": (20.5235, -100.8159, "America/Mexico_City"),  # MX
-    "Zacatecas": (22.7709, -102.5832, "America/Mexico_City"),  # MX
-    "San Juan del Rio": (20.3875, -99.9962, "America/Mexico_City"),  # MX
-    "San Luis Potosi": (22.1498, -100.9792, "America/Mexico_City"),  # MX
-    "Durango": (24.0277, -104.6532, "America/Mexico_City"),  # MX
-    "Córdoba": (18.8941, -96.9347, "America/Mexico_City"),  # MX  <-- CIUDAD AÑADIDA
-    "Irapuato": (20.6773, -101.3551, "America/Mexico_City"),  # MX
+    "Tuxtla Gutierrez": (16.7510, -93.1160, "America/Mexico_City"),
+    "Ensenada": (31.8667, -116.6000, "America/Tijuana"),
+    "Celaya": (20.5235, -100.8159, "America/Mexico_City"),
+    "Zacatecas": (22.7709, -102.5832, "America/Mexico_City"),
+    "San Juan del Rio": (20.3875, -99.9962, "America/Mexico_City"),
+    "San Luis Potosi": (22.1498, -100.9792, "America/Mexico_City"),
+    "Durango": (24.0277, -104.6532, "America/Mexico_City"),
+    "Irapuato": (20.6773, -101.3551, "America/Mexico_City"),
 
     # 🦸 Heros (Mexico)
-    "Nuevo Laredo": (27.4763, -99.5164, "America/Monterrey"),  # MX
-    "Reynosa": (26.0923, -98.2773, "America/Monterrey"),  # MX
-    "Victoria": (23.7369, -99.1411, "America/Monterrey"),  # MX
-    "Morelia": (19.7008, -101.1844, "America/Mexico_City"),  # MX
-    "La Paz": (24.1426, -110.3128, "America/Mazatlan"),  # MX
-    "Matamoros": (25.8697, -97.5027, "America/Monterrey"),  # MX
-    "Manzanillo": (19.0523, -104.3157, "America/Mexico_City"),  # MX
+    "Nuevo Laredo": (27.4763, -99.5164, "America/Monterrey"),
+    "Reynosa": (26.0923, -98.2773, "America/Monterrey"),
+    "Victoria": (23.7369, -99.1411, "America/Monterrey"),
+    "Morelia": (19.7008, -101.1844, "America/Mexico_City"),
+    "La Paz": (24.1426, -110.3128, "America/Mazatlan"),
+    "Matamoros": (25.8697, -97.5027, "America/Monterrey"),
+    "Manzanillo": (19.0523, -104.3157, "America/Mexico_City"),
 
     # 🏫 POC Academy (Chile, Mexico, Costa Rica)
-    "Chillan": (-36.6066, -72.1027, "America/Santiago"),  # CL
-    "Los Angeles": (-37.4697, -72.3537, "America/Santiago"),  # CL
-    "Los Andes-San Felipe": (-32.8333, -70.5984, "America/Santiago"),  # CL
-    "Temuco": (-38.7359, -72.5904, "America/Santiago"),  # CL
-    "Puerto Montt": (-41.4717, -72.9364, "America/Santiago"),  # CL
-    "Magallanes": (-53.1638, -70.9171, "America/Punta_Arenas"),  # CL
-    "Ovalle": (-30.5980, -71.2005, "America/Santiago"),  # CL
-    "Arica": (-18.4783, -70.3211, "America/Santiago"),  # CL
-    "Copiapo": (-27.3668, -70.3322, "America/Santiago"),  # CL
-    "Osorno": (-40.5740, -73.1335, "America/Santiago"),  # CL
-    "Antofagasta": (-23.6524, -70.3954, "America/Santiago"),  # CL
-    "Iquique": (-20.2141, -70.1523, "America/Santiago"),  # CL
-    "Calama": (-22.4560, -68.9231, "America/Santiago"),  # CL
-    "Liberia": (10.6333, -85.4333, "America/Costa_Rica"),  # CR
-    "San Carlos": (10.3204, -84.4263, "America/Costa_Rica"),  # CR
-    "Navojoa": (27.0701, -109.4431, "America/Hermosillo"),  # MX
-    "Tepic": (21.5076, -104.8932, "America/Mazatlan"),  # MX
+    "Chillan": (-36.6066, -72.1027, "America/Santiago"),
+    "Los Angeles": (-37.4697, -72.3537, "America/Santiago"),
+    "Los Andes-San Felipe": (-32.8333, -70.5984, "America/Santiago"),
+    "Temuco": (-38.7359, -72.5904, "America/Santiago"),
+    "Puerto Montt": (-41.4717, -72.9364, "America/Santiago"),
+    "Magallanes": (-53.1638, -70.9171, "America/Punta_Arenas"),
+    "Ovalle": (-30.5980, -71.2005, "America/Santiago"),
+    "Arica": (-18.4783, -70.3211, "America/Santiago"),
+    "Copiapo": (-27.3668, -70.3322, "America/Santiago"),
+    "Osorno": (-40.5740, -73.1335, "America/Santiago"),
+    "Antofagasta": (-23.6524, -70.3954, "America/Santiago"),
+    "Iquique": (-20.2141, -70.1523, "America/Santiago"),
+    "Calama": (-22.4560, -68.9231, "America/Santiago"),
+    "Liberia": (10.6333, -85.4333, "America/Costa_Rica"),
+    "San Carlos": (10.3204, -84.4263, "America/Costa_Rica"),
+    "Navojoa": (27.0701, -109.4431, "America/Hermosillo"),
+    "Tepic": (21.5076, -104.8932, "America/Mazatlan"),
 
     # 🔬 POC LAB (Mexico, Peru, Argentina, Panama)
-    "Panama City": (8.9833, -79.5167, "America/Panama"),  # PA
-    "La Plata": (-34.9215, -57.9545, "America/Argentina/Buenos_Aires"),  # AR
-    "Cusco": (-13.5320, -71.9675, "America/Lima"),  # PE
-    "Arequipa": (-16.3989, -71.5350, "America/Lima"),  # PE
-    "Veracruz": (19.1783, -96.1342, "America/Mexico_City"),  # MX
-    "Coatzacoalcos": (18.1366, -94.4401, "America/Mexico_City"),  # MX
-    "Xalapa": (19.5438, -96.9108, "America/Mexico_City"),  # MX
-    "Villahermosa": (17.9869, -92.9303, "America/Mexico_City"),  # MX
-    "Los Mochis-Guasave": (25.7969, -108.9916, "America/Mazatlan"),  # MX
-    "Vallarta": (20.6534, -105.2253, "America/Mexico_City"),  # MX
-    "Tlaxcala": (19.3142, -98.2400, "America/Mexico_City"),  # MX
-    "Guaymas": (27.9214, -110.8977, "America/Hermosillo"),  # MX
-    "San Miguel de Allende": (20.9140, -100.7436, "America/Mexico_City"),  # MX
+    "Panama City": (8.9833, -79.5167, "America/Panama"),
+    "La Plata": (-34.9215, -57.9545, "America/Argentina/Buenos_Aires"),
+    "Cusco": (-13.5320, -71.9675, "America/Lima"),
+    "Arequipa": (-16.3989, -71.5350, "America/Lima"),
+    "Veracruz": (19.1783, -96.1342, "America/Mexico_City"),
+    "Córdoba": (18.8941, -96.9347, "America/Mexico_City"),
+    "Coatzacoalcos": (18.1366, -94.4401, "America/Mexico_City"),
+    "Xalapa": (19.5438, -96.9108, "America/Mexico_City"),
+    "Villahermosa": (17.9869, -92.9303, "America/Mexico_City"),
+    "Los Mochis-Guasave": (25.7969, -108.9916, "America/Mazatlan"),
+    "Vallarta": (20.6534, -105.2253, "America/Mexico_City"),
+    "Tlaxcala": (19.3142, -98.2400, "America/Mexico_City"),
+    "Guaymas": (27.9214, -110.8977, "America/Hermosillo"),
+    "San Miguel de Allende": (20.9140, -100.7436, "America/Mexico_City"),
 
     # 🚀 Rocket (Chile, Colombia, Ecuador, Mexico, Republica Dominicana)
-    "La Serena-Coquimbo": (-29.9027, -71.2520, "America/Santiago"),  # CL
-    "Concepcion": (-36.8270, -73.0503, "America/Santiago"),  # CL
-    "Rancagua-San Fernando": (-34.1708, -70.7444, "America/Santiago"),  # CL
-    "Talca-Curico-Linares": (-35.4232, -71.6554, "America/Santiago"),  # CL
-    "Valdivia": (-39.8142, -73.2459, "America/Santiago"),  # CL
-    "Ibague": (4.4389, -75.2322, "America/Bogota"),  # CO
-    "Pereira": (4.8143, -75.6946, "America/Bogota"),  # CO
-    "Cucuta": (7.8939, -72.5078, "America/Bogota"),  # CO
-    "Popayan": (2.4382, -76.6132, "America/Bogota"),  # CO
-    "Villavicencio": (4.1420, -73.6266, "America/Bogota"),  # CO
-    "Santiago de los Caballeros": (19.4500, -70.7000, "America/Santo_Domingo"),  # DO
-    "Guayaquil": (-2.1894, -79.8891, "America/Guayaquil"),  # EC
-    "Ciudad Obregon": (27.4864, -109.9408, "America/Hermosillo"),  # MX
-    "Colima": (19.2433, -103.7250, "America/Mexico_City"),  # MX
+    "La Serena-Coquimbo": (-29.9027, -71.2520, "America/Santiago"),
+    "Concepcion": (-36.8270, -73.0503, "America/Santiago"),
+    "Rancagua-San Fernando": (-34.1708, -70.7444, "America/Santiago"),
+    "Talca-Curico-Linares": (-35.4232, -71.6554, "America/Santiago"),
+    "Valdivia": (-39.8142, -73.2459, "America/Santiago"),
+    "Ibague": (4.4389, -75.2322, "America/Bogota"),
+    "Pereira": (4.8143, -75.6946, "America/Bogota"),
+    "Cucuta": (7.8939, -72.5078, "America/Bogota"),
+    "Popayan": (2.4382, -76.6132, "America/Bogota"),
+    "Villavicencio": (4.1420, -73.6266, "America/Bogota"),
+    "Santiago de los Caballeros": (19.4500, -70.7000, "America/Santo_Domingo"),
+    "Guayaquil": (-2.1894, -79.8891, "America/Guayaquil"),
+    "Ciudad Obregon": (27.4864, -109.9408, "America/Hermosillo"),
+    "Colima": (19.2433, -103.7250, "America/Mexico_City"),
 }
+
+cities.update({
+    "Mexico City": (19.4326, -99.1332, "America/Mexico_City"),
+    "Guadalajara": (20.6597, -103.3496, "America/Mexico_City"),
+    "Monterrey": (25.6866, -100.3161, "America/Monterrey")
+})
